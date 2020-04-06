7ebbe85fb9009b491da08ec167e44121e109e8b2
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-04-06 11:08:35 +0200

    Remove manual data corrections and add CoronaDataScraper data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,46 +1,26 @@
 import pandas as pd
 
 
-def apply_data_corrections(dataframe_confirmed_DF):
-    # https://github.com/CSSEGISandData/COVID-19/issues/833
-    confirmed_fixes_dict = {
-        'Italy|3/12/20': 15113,
-        'Spain|3/12/20': 3146,
-        'France|3/12/20': 2876,
-        'United Kingdom|3/12/20': 590,
-        'Germany|3/12/20': 2745,
-        'Argentina|3/12/20': 19,
-        'Belgium|3/12/20': 314,
-        'Chile|3/12/20': 23,
-        'Greece|3/12/20': 98,
-        'Indonesia|3/12/20': 34,
-        'Ireland|3/12/20': 43,
-        'Japan|3/12/20': 620,
-        'Netherlands|3/12/20': 503,
-        'Qatar|3/12/20': 262,
-        'Singapore|3/12/20': 178,
-        'United Kingdom|3/12/20': 1391,
-        'France|3/15/20': 5423,
-        'France|4/04/20': 68605
-    }
-    for key in confirmed_fixes_dict.keys():
-        country_to_be_fixed = key.split('|')[0]
-        date_to_be_fixed = key.split('|')[1]
-        value_to_be_fixed = confirmed_fixes_dict[key]
-        dataframe_confirmed_DF.at[country_to_be_fixed,
-                                  date_to_be_fixed] = value_to_be_fixed
-    return dataframe_confirmed_DF
-
-
 def load_jhu_data():
     data = pd.read_csv(
         'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
         index_col=1)
-    data = apply_data_corrections(data)
     data.index = data.index + (" (" + data['Province/State'] + ")").fillna("")
     del data["Province/State"]
     del data["Lat"]
     del data["Long"]
     data = data.transpose()
     data.index = pd.to_datetime(data.index)
+    return data
+
+
+def load_cds_data(level='country', upper_levels=()):
+    levels = ['country', 'state', 'county', 'city']
+    assert level in levels, "Invalid level '{}'. Available: {}".format(level, levels)
+    data = pd.read_csv(
+        'https://coronadatascraper.com/timeseries.csv',
+        index_col=['level'] + levels + ['date'],
+        parse_dates=['date'])
+    data = data.loc[(level,) + tuple(upper_levels)]
+    data.index = data.index.droplevel(levels[len(upper_levels) + 1:])
     return data
